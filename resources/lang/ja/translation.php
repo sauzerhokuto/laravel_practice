572d9e3fb518b7c1396789c409f6f7b8268f77fa
野崎大生 <sauzerhokuto@gmail.com>
2023-03-17 17:37:37 +0900

    各種追記と修正を行う。

--- resources/lang/ja/translation.php
+++ resources/lang/ja/translation.php
@@ -12,4 +12,9 @@ return [
   'Reset Password' => 'パスワードをリセット',
   'Send Password Reset Link' => 'メールアドレスに送信',
   'Please confirm your password before continuing.' => '続行する前にパスワードを確認してください。',
+
+  // 記事登録
+  'Title' => 'タイトル',
+  'Body' => '本文',
+  'Post' => '投稿',
 ];
